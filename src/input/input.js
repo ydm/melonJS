3fe7a314c368449ff66f3865bb1c3c47684386ef
Olivier Biot <olivier.biot@me.com>
2018-04-16 09:52:06 +0800

    me.input work in progress (uncomplete)

--- src/input/input.js
+++ src/input/input.js
@@ -4,57 +4,46 @@
  * http://www.melonjs.org/
  *
  */
-(function () {
-    /**
-     * @namespace me.input
-     * @memberOf me
-     */
-    me.input = (function () {
-        // hold public stuff in our singleton
-        var api = {};
 
-        /*
-         * PRIVATE STUFF
-         */
+/**
+ * @namespace me.input
+ * @memberOf me
+ */
 
-        /**
-         * prevent event propagation
-         * @ignore
-         */
-        api._preventDefaultFn = function (e) {
-            // stop event propagation
-            if (e.stopPropagation) {
-                e.stopPropagation();
-            }
-            else {
-                e.cancelBubble = true;
-            }
-            // stop event default processing
-            if (e.preventDefault)  {
-                e.preventDefault();
-            }
-            else  {
-                e.returnValue = false;
-            }
 
-            return false;
-        };
+/**
+ * prevent event propagation
+ * @ignore
+ */
+export function _preventDefaultFn(e) {
+    // stop event propagation
+    if (e.stopPropagation) {
+        e.stopPropagation();
+    }
+    else {
+        e.cancelBubble = true;
+    }
+    // stop event default processing
+    if (e.preventDefault)  {
+        e.preventDefault();
+    }
+    else  {
+        e.returnValue = false;
+    }
 
-        /*
-         * PUBLIC STUFF
-         */
+    return false;
+};
 
-        /**
-         * Global flag to specify if melonJS should prevent all default browser action on registered events.
-         * default : true
-         * @public
-         * @type Boolean
-         * @name preventDefault
-         * @memberOf me.input
-         */
-        api.preventDefault = true;
+/*
+ * PUBLIC STUFF
+ */
 
-        // return our object
-        return api;
-    })();
-})();
+/**
+ * Global flag to specify if melonJS should prevent all default browser action on registered events.
+ * default : true
+ * @public
+ * @type Boolean
+ * @name preventDefault
+ * @memberOf me.input
+ */
+export let preventDefault = true;
